815405226cb6cbebd0831dcd8d7b50f33e4d3978
KSHITIZMISHRA31 <46749989+KSHITIZMISHRA31@users.noreply.github.com>
2020-03-12 00:57:52 +0530

    Aerospace Activity created

---
+++ app/src/main/java/com/example/linkedpath/Aerospace_eng.java
@@ -0,0 +1,14 @@
+package com.example.linkedpath;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Aerospace_eng extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aerospace_eng);
+    }
+}
---
+++ app/src/main/java/com/example/linkedpath/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.linkedpath;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.Instant;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=(TextView)findViewById(R.id.textView2);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Aerospace_eng.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "You Clicked on ", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
